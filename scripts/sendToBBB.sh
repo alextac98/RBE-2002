bab531f4f38a674dde249b43ca6dae1c6cee4c33
Alex Tacescu <alextac98@gmail.com>
2017-12-11 22:33:47 -0500

    modify sendToBBB script

--- scripts/sendToBBB.sh
+++ scripts/sendToBBB.sh
@@ -1,1 +1,1 @@
-rsync -r --progress --delete-after /home/alex/RBE-2002/src root@192.168.7.2:/root/RBE-2002/
+rsync -r --progress --delete-after /home/alex/RBE-2002/src/turtlebotmini root@192.168.7.2:/root/RBE-2002/src/
