f6e24559c9f25d1fdd762cbeadd00f46e195f2c7
Alex Tacescu <alextac98@gmail.com>
2017-12-16 02:15:44 -0500

    final Working robot

--- src/euclidkit/rbe2002_bringup/src/littleguy.cpp
+++ src/euclidkit/rbe2002_bringup/src/littleguy.cpp
@@ -13,34 +13,35 @@ int main(int argc, char **argv)
 
     ros::Publisher fan = nh.advertise<std_msgs::Float32>("fanSpeed", 1);
 
-//    MoveBaseClient ac("move_base", true);
+    MoveBaseClient ac("move_base", true);
 
-//    while(!ac.waitForServer(ros::Duration(5.0))){
-//       ROS_INFO("Waiting for the move_base action server to come up");
-//     }
+    while(!ac.waitForServer(ros::Duration(5.0))){
+       ROS_INFO("Waiting for the move_base action server to come up");
+     }
 
-//    move_base_msgs::MoveBaseGoal goal;
+    move_base_msgs::MoveBaseGoal goal;
 
-//    goal.target_pose.header.frame_id = "base_link";
-//    goal.target_pose.header.stamp = ros::Time::now();
+    goal.target_pose.header.frame_id = "base_link";
+    goal.target_pose.header.stamp = ros::Time::now();
 
-//    goal.target_pose.pose.position.x = .667;
-//    goal.target_pose.pose.position.y = .333;
-//    goal.target_pose.pose.orientation.w = 1;
+    goal.target_pose.pose.position.x = .667;
+    goal.target_pose.pose.position.y = .333;
+    goal.target_pose.pose.orientation.w = 1;
 
-//    ROS_INFO("Sending Goal");
-//    ac.sendGoal(goal);
+    ROS_INFO("Sending Goal");
+    ac.sendGoal(goal);
 
-//    //only for demo
-//    ac.waitForResult();
+    ac.waitForResult();
 
-//    if (ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED) {
+    if (ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED) {
         std_msgs::Float32 fanPower;
         fanPower.data = 0.5;
         fan.publish(fanPower);
 
-        ROS_INFO("Fire Position X:56.8376inches, Y:-28.997inches, Z:8.254inches");
-//    }
+        ROS_INFO("Fire Position X:" + pub_image.getX() + "inches" +
+                 "Y:" + pub_image.getY() + "inches" +
+                 "Z:" + pub_image.getZ() + "inches");
+    }
 
 //    goal.target_pose.pose.position.x = 0;
 //    goal.target_pose.pose.position.y = 0;
@@ -49,5 +50,5 @@ int main(int argc, char **argv)
 
 //    ac.waitForResult();
 
-//    ROS_INFO("Im Home!");
+    ROS_INFO("Im Home!");
 }
